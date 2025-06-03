46708b4a69e40d4eaed53478b4d47c0ddb80b2ee
SudacaDev <sudacadev@gmail.com>
2025-06-03 11:54:41 -0300

    space added

--- web/src/hooks/useAuthGuard.ts
+++ web/src/hooks/useAuthGuard.ts
@@ -3,8 +3,6 @@ import { USER_TYPE } from '../../../helper/types/user.type.ts';
 
 export const useAuthGuard = (allowedRoles: USER_TYPE[]) => {
   const { isAuth, role } = useSessionStore();
-
   const canAccess = isAuth && role && allowedRoles.includes(role);
-
   return canAccess;
 };
