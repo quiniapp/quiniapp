7484308e685bda86b3d0f6fe0edec974cf031f31
AgustinEze <agustineg@gmail.com>
2025-06-11 15:29:16 -0300

    vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,10 @@
 import path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss(), // Usando la importación directa
-        autoprefixer(), // Usando la importación directa
-      ],
-    },
-  },
+
   server: {
     hmr: {
       overlay: false,
