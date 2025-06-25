22bd9587ce83fcf68b6791118c381b29e6b60b8f
AgustinEze <agustineg@gmail.com>
2025-06-25 15:24:41 -0300

    error return api

--- api/src/auth/route/auth.route.ts
+++ api/src/auth/route/auth.route.ts
@@ -186,10 +186,10 @@ export class AuthRouter {
         },
       };
 
-      return res.status(200).json(response);
+      res.status(200).json(response);
     } catch (error) {
       console.error('Error in /refresh route', error);
-      return res.status(500).json({
+      res.status(500).json({
         error: {
           error: ERROR_TYPE.INTERNAL_SERVER_ERROR,
           message: 'Unexpected error',
