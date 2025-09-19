ebd98c28ec59f9b7b8f1b99c4a0a75732e52dfb2
AgustinEze <agustineg@gmail.com>
2025-09-19 14:24:31 -0300

    enviroment back

--- api/envs.ts
+++ api/envs.ts
@@ -3,6 +3,8 @@ import 'dotenv/config';
 const ENVIROMENT = process.env.ENVIROMENT;
 const DATABASE_ENVIROMENT = process.env.SUPABASE_ENVIROMENT;
 
+const FRONT_URL_ENVIROMENT = process.env.FRONT_URL_ENVIROMENT;
+
 export const JWT_SECRET_USER = process.env.JWT_SECRET_USER!;
 //api
 export const PORT = ENVIROMENT === 'LOCAL' ? 3000 : process.env.PORT;
@@ -20,3 +22,10 @@ export const JWT_SECRET_SUPABASE =
   DATABASE_ENVIROMENT === 'LOCAL'
     ? process.env.JWT_SECRET_SUPABASE_LOCAL
     : process.env.JWT_SECRET_SUPABASE!;
+
+export const FRONT_URL =
+  FRONT_URL_ENVIROMENT === 'PRODUCTION'
+    ? process.env.FRONT_ENVIROMET_PRODUCTION
+    : FRONT_URL_ENVIROMENT === 'DEVELOP'
+      ? process.env.FRONT_ENVIROMET_DEVELOP
+      : 'http://localhost:5173';
