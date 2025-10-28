62f1822ae6f9f45dcd51210c1ce2039469b3f5f6
AgustinEze <agustineg@gmail.com>
2025-10-28 08:17:41 -0300

    order in ticket terminal

--- web/src/features/terminal-ticket/index.tsx
+++ web/src/features/terminal-ticket/index.tsx
@@ -51,7 +51,7 @@ export const TerminalTicketContent = () => {
     <PageWrapper>
       <HeaderSection title={'Revisar Tickets'} className={'w-full sticky top-0'} />
       <FlexCol className={'1440:py-[36px] py-[16px]'}>
-        <FlexCol className={'sm:flex gap-8'}>
+        <FlexCol className={'sm:flex sm:flex-row gap-8'}>
           <FormHeaderFilter />
           <FlexCol>
             <FlexCol>
