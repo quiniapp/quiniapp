d841aad5251a9c9ad9e26faf14c08adb2216efb5
AgustinEze <agustineg@gmail.com>
2025-12-07 23:32:00 -0300

    add analytics

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './pages/App.tsx';
 import { injectSpeedInsights } from '@vercel/speed-insights';
- 
+ import { inject } from "@vercel/analytics"
+
 injectSpeedInsights();
+inject()
 
 createRoot(document.getElementById('root')!).render(<App />);
