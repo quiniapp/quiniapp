0003ee453d020c25777a48dbe467c9ffa19c6d3a
AgustinEze <agustineg@gmail.com>
2025-12-16 21:34:20 -0300

    only show availables lotteries on fill ticket

--- web/src/features/make-plays/game-turns.tsx
+++ web/src/features/make-plays/game-turns.tsx
@@ -4,11 +4,15 @@ import ScheduleCheckboxList from '@/features/make-plays/schedules-checkbox-list'
 import { ILotteryEntityFront } from '@helper/types/lottery.type';
 import { IScheduleEntityFront } from '@helper/types/schedule.type';
 import { useClock } from '@/providers/ClockProvider';
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { USER_TYPE } from '@helper/types/user.type';
 import { useAuth } from '@/contexts/AuthContext';
 import LotteriesCheckboxList from './lotteries-checkbox-list';
 import { usePlayDetails } from './context/MakePlaysContext';
+import { useScheduleLottery } from '@/hooks/fetchs/schedule-lottery/useScheduleLottery';
+import { DayKey } from '@helper/types/schedule-lottery.type';
+import { dayParseToString } from '@helper/functions/dayDictionary';
+import dayjs from 'dayjs';
 
 const GameTurns = () => {
   const {
@@ -19,6 +23,7 @@ const GameTurns = () => {
     setIsEnabledCreateBet,
   } = usePlayDetails();
 
+  const today = dayjs().day();
   const handleSchedules = (schedule: IScheduleEntityFront) => {
     setSchedules((prev) => {
       const newMap = new Map(prev);
@@ -45,6 +50,19 @@ const GameTurns = () => {
   const { now, isLessThanTenMinutes } = useClock();
   const { role } = useAuth();
   const { data: schedulesData } = useSchedules();
+  const { data: scheduleLotteryPerDate } = useScheduleLottery();
+
+  const schedulesAvailables = useMemo(() => {
+    if (!schedulesData?.length) return [];
+
+    const todayKey: DayKey = dayParseToString[today];
+    const daySchedules = scheduleLotteryPerDate?.[todayKey]; // Record<string, string[]> | undefined
+    if (!daySchedules) return [];
+
+    const ids = new Set(Object.keys(daySchedules)); // schedule_id que tienen loterías hoy
+
+    return schedulesData.filter((sch) => ids.has(sch.schedule_id));
+  }, [schedulesData, scheduleLotteryPerDate, today]);
 
   useEffect(() => {
     const status = schedulesData?.some((sch: IScheduleEntityFront) =>
@@ -56,7 +74,7 @@ const GameTurns = () => {
   return (
     <Flex className="grid grid-cols-2 gap-2 lg:gap-1.5 sm:flex sm:flex-col 1440:space-y-5 sm:flex-1 sm:justify-between">
       <ScheduleCheckboxList
-        schedules={schedulesData ?? []}
+        schedules={schedulesAvailables}
         setSchedules={handleSchedules}
         checkedSchedules={checkedSchedules}
       />
